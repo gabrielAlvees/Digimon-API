85b804e4cfd5090e9a23bdf6473c93bed08e042b
gabrielAlvees <gabrielDev.alves@gmail.com>
2023-02-04 12:01:38 -0300

    feat: new component sales

---
+++ src/pages/Sales/data/DataSales.ts
@@ -0,0 +1,39 @@
+import imageSale01 from '../../../assets/1235.jpg';
+import imageSale02 from '../../../assets/1150.jpg';
+import imageSale03 from '../../../assets/1120.jpg';
+import imageSale04 from '../../../assets/1237.jpg';
+import imageSale05 from '../../../assets/1150.jpg';
+import imageSale06 from '../../../assets/1151.jpg';
+
+export const DataSales = [
+  {
+    imageSale: imageSale01,
+    date: '2023/02/03',
+    about: 'Digimon Card Game Rising Wind [RB-01] Released on Friday, January 27! Grab victory with the rebooted card!'
+  },
+  {
+    imageSale: imageSale02,
+    date: '2023/04/12',
+    about: 'Amphimon and Cutiramon are newly added to the Digimon Encyclopedia!'
+  },
+  {
+    imageSale: imageSale03,
+    date: '2023/07/26',
+    about: 'Special site "Digimon Journal" updated! This time, "Jesmon", "Gankoomon" and "Espimon" are picked up!'
+  },
+  {
+    imageSale: imageSale04,
+    date: '2023/06/01',
+    about: 'Gabumon and Patamon appear from the "Rukappu" series of cute figures that look up!'
+  },
+  {
+    imageSale: imageSale05,
+    date: '2023/12/20',
+    about: '"Digimon Ghost Game" soundtrack CD jacket photo released!'
+  },
+  {
+    imageSale: imageSale06,
+    date: '2023/01/30',
+    about: 'Special site "Digimon Journal" updated! This time, "Omegamon Alter-S", "WereGarurumon" and "Lilithmon" are picked up!'
+  },
+]
---
+++ src/pages/Sales/styles.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const SectionAbout = styled.div`
+  background-color: #fff;
+  width: 100%;
+`;
+
+export const Title = styled.p`
+  font-family: var(--font-cairo);
+  font-size: 2.5rem;
+  text-align: center;
+  color: #1251d0;
+  padding: 40px 0;
+`;
+
+export const ContainerSalesMain = styled.div`
+  width: 85%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 0 10px;
+`;
+
+export const ButtonSales = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  width: 100%;
+  background-color: transparent;
+  border: none;
+`;
+
+export const ImageSales = styled.img`
+  width: 16.80rem;
+  height: 11rem;
+`;
+
+export const ContainerAbout = styled.div``
+
+export const About = styled.p`
+  text-align: start;
+  padding: 5px 10px;
+`;
+
+export const Date = styled.p`
+  text-align: start;
+  padding: 5px 10px;
+`;
