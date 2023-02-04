4b7905604c22aa05bc9446be905c148a9c60237e
gabrielAlvees <gabrielDev.alves@gmail.com>
2023-02-04 12:01:59 -0300

    feat: call component sales and map in page sales

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,13 @@
-// React
-import { useState } from "react";
-
 // Importando Componentes
 import { Navbar } from "../../components/Navbar";
+import { Sales } from "../Sales";
 
 // Importando Imagens
 import gifImage from "../../assets/header_bg.jpg";
 import digimonImage from "../../assets/hero_img.png";
-import bgAbout from "../../assets/bgHomeSection2.jpg";
-import imageSales from "../../assets/1120.jpg";
 
 // Importando Estilos
-import {
-  Main,
-  HomeImage,
-  ImageDigimon,
-  DigimonBackground,
-  SectionAbout,
-  Title,
-  ContainerSalesMain,
-  ButtonSales,
-  ImageSales,
-  ContainerAbout,
-  About,
-  Date,
-} from "./styles";
+import { Main, HomeImage, ImageDigimon, DigimonBackground } from "./styles";
 
 export const Home = () => {
   return (
@@ -34,22 +17,7 @@ export const Home = () => {
         <ImageDigimon src={gifImage} />
         <DigimonBackground src={digimonImage} />
       </HomeImage>
-      <SectionAbout>
-        <Title>Latest updates</Title>
-        <ContainerSalesMain>
-          <ButtonSales>
-            <ImageSales src={imageSales} />
-            <ContainerAbout>
-              <Date>yyyy/DD/mm</Date>
-              <About>
-                Digimon Card Game VS Royal Knights [BT-13] Scheduled to be
-                released on 02/24 (Fri.)! Gather 13 Holy Knights! Burst mode
-                also appeared for the first time! !
-              </About>
-            </ContainerAbout>
-          </ButtonSales>
-        </ContainerSalesMain>
-      </SectionAbout>
+      <Sales />
     </Main>
   );
 };
