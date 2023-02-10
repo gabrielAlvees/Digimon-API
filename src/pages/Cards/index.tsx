c783af9138b48a6ed2d89fd2ed70efd55d604f96
gabrielAlvees <gabrielDev.alves@gmail.com>
2023-02-09 21:58:56 -0300

    feat: new page cards

---
+++ src/pages/Cards/index.tsx
@@ -0,0 +1,13 @@
+// Importando Componentes
+
+// Importando Estilos
+import { Main, Links, ContainerCards } from "./styles";
+
+export const Cards = () => {
+  return (
+    <Main>
+      <Links></Links>
+      <ContainerCards></ContainerCards>
+    </Main>
+  );
+};
---
+++ src/pages/Cards/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Links = styled.div`
+  border: 4px solid red;
+  height: 30vh;
+  width: 50vh;
+  /* Deixar fixo */
+`;
+
+export const ContainerCards = styled.div`
+  border: 4px solid blue;
+  height: 100vh;
+  width: 100vh;
+`;
