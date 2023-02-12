a7bca70b824a397988544836de69dcc9e9e6bf8f
gabrielAlvees <gabrielDev.alves@gmail.com>
2023-02-12 17:56:38 -0300

    feat: container filter and input with onchange value for search digimon

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,13 +1,78 @@
-// Importando Componentes
+// React
+import { useEffect, useState } from "react";
 
-// Importando Estilos
-import { Main, Links, ContainerCards } from "./styles";
+// Importando componentes
+import { ICards } from "../../types/Cards";
+
+// API
+import axios from "axios";
+
+// CSS
+import {
+  Main,
+  FlexContainer,
+  ContainerFilter,
+  Title,
+  InputNameDigimon,
+  ContainerCards,
+  ContainerImg,
+  LoadMoreButton,
+  CardsImage,
+} from "./styles";
 
 export const Cards = () => {
+  const [listCards, setListCards] = useState<ICards[]>([]);
+
+  const getCards = async () => {
+    try {
+      const response = await axios.get(
+        `https://digimoncard.io/api-public/search.php?`,
+        { params: { n: nameDigimon || "" } }
+      );
+      setListCards(response.data);
+    } catch (error: any) {
+      console.log(`${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getCards();
+  }, []);
+
+  // Load More
+  const [cardsToShow, setCardsToShow] = useState(8);
+  const handleLoadMore = () => {
+    setCardsToShow(cardsToShow + 8);
+  };
+
+  // Filter Name
+  const [nameDigimon, setNameDigimon] = useState("");
+
   return (
     <Main>
-      <Links></Links>
-      <ContainerCards></ContainerCards>
+      <FlexContainer>
+        <ContainerFilter>
+          <Title>Filters</Title>
+          <InputNameDigimon
+            type="text"
+            value={nameDigimon}
+            onChange={(e) => setNameDigimon(e.target.value)}
+          />
+          <button onClick={() => getCards()}>A</button>
+        </ContainerFilter>
+        <ContainerCards>
+          {listCards.slice(0, cardsToShow).map((cards: ICards) => (
+            <ContainerImg>
+              <CardsImage src={`${cards.image_url}`} />
+              <div>a</div>
+            </ContainerImg>
+          ))}
+        </ContainerCards>
+      </FlexContainer>
+
+      {listCards.length > cardsToShow && (
+        <LoadMoreButton onClick={handleLoadMore}>Carregar mais</LoadMoreButton>
+      )}
     </Main>
   );
 };
