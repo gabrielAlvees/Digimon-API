a26b964eb481dd786ac323595dd7b3a59c8a960b
gabrielAlvees <gabrielDev.alves@gmail.com>
2023-02-12 17:56:02 -0300

    feat: new styles and container filter style

--- src/pages/Cards/styles.ts
+++ src/pages/Cards/styles.ts
@@ -1,19 +1,41 @@
 import styled from "styled-components";
 
-export const Main = styled.div`
+export const Main = styled.div``;
+
+export const FlexContainer = styled.div`
   display: flex;
-  align-items: stretch;
 `;
 
-export const Links = styled.div`
+export const ContainerFilter = styled.div`
   border: 4px solid red;
-  height: 30vh;
-  width: 50vh;
-  /* Deixar fixo */
+  width: 30vw;
+  margin-right: 20px;
 `;
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const InputNameDigimon = styled.input``;
 
 export const ContainerCards = styled.div`
   border: 4px solid blue;
-  height: 100vh;
-  width: 100vh;
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const ContainerImg = styled.div`
+  margin: 10px;
+  background-color: #000;
+  border: 2px solid red;
+`;
+
+export const LoadMoreButton = styled.button``;
+
+export const CardsImage = styled.img`
+  width: 15rem;
+  height: 20rem;
 `;
