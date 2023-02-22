317e1513c24413292451bd18de078645e3ebaab1
gabrielAlvees <gabrielDev.alves@gmail.com>
2023-02-21 22:21:28 -0300

    feat: added toastfy

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import ReactDOM from "react-dom/client";
 import { NavigateRoutes } from "./routes";
 import GlobalStyles from "./styles/global";
 
+// Toast
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <NavigateRoutes />
     <GlobalStyles />
+    <ToastContainer />
   </>
 );
