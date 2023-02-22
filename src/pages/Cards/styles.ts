d6e6714666f386fa8de3d8a00ca7e3f1ffba124e
gabrielAlvees <gabrielDev.alves@gmail.com>
2023-02-21 22:20:45 -0300

    feat: container flex and new styles for cards

--- src/pages/Cards/styles.ts
+++ src/pages/Cards/styles.ts
@@ -4,24 +4,59 @@ export const Main = styled.div``;
 
 export const FlexContainer = styled.div`
   display: flex;
+  align-items: flex-start;
 `;
 
 export const ContainerFilter = styled.div`
-  border: 4px solid red;
   width: 30vw;
   margin-right: 20px;
+  padding: 10px;
 `;
 
 export const Title = styled.h1`
   text-align: center;
 `;
 
-export const InputNameDigimon = styled.input``;
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
 
-export const ButtonSearch = styled.button``;
+`;
+
+export const InputNameDigimon = styled.input`
+  outline: none;
+
+
+`;
+
+export const Label = styled.label`
+  font-family: var(--font-ele);
+  margin-top: 10px;
+  margin-bottom: 5px;
+`;
+
+export const Select = styled.select`
+  padding: 5px 2px;
+  background-color: transparent;
+  border: 1px solid #252422;
+`;
+
+export const Option = styled.option``;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  justify-content: end;
+  margin-top: 10px;
+`;
+
+export const ButtonSearch = styled.button`
+  width: 100%;
+  background-color: #7eff20;
+  border: none;
+  padding: 5px;
+`;
 
 export const ContainerCards = styled.div`
-  border: 4px solid blue;
   width: 70%;
   margin: 0 auto;
   display: flex;
@@ -31,9 +66,11 @@ export const ContainerCards = styled.div`
 
 export const ContainerImg = styled.div`
   margin: 10px;
-  background-color: #000;
-  border: 2px solid red;
+  display: flex;
+  justify-content: end;
+  align-items: flex-start;
 `;
+
 
 export const LoadMoreButton = styled.button``;
 
@@ -41,3 +78,4 @@ export const CardsImage = styled.img`
   width: 15rem;
   height: 20rem;
 `;
+
